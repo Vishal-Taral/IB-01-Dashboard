f1d9f19616a96ccef91fd0084bd5c365890c1d6f
Vishal-Taral <vishaltaral09@gmail.com>
2022-06-16 20:10:42 +0530

    logic building

--- Dashboard/src/app/app-routing.module.ts
+++ Dashboard/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateProfileComponent } from './components/create-profile/create-profile.component';
 import{DashboardComponent} from './components/dashboard/dashboard.component';
 import { LogInComponent } from './components/log-in/log-in.component';
 
 const routes: Routes = [
 
   {path:'dashboard',component:DashboardComponent},
-  { path:'log-in',component:LogInComponent}
+  { path:'log-in',component:LogInComponent},
+  {path:'create-profile', component: CreateProfileComponent}
 ];
 
 @NgModule({
--- Dashboard/src/app/app.module.ts
+++ Dashboard/src/app/app.module.ts
@@ -1,34 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { UserDataComponent } from './components/user-data/user-data.component';
-import { LogInComponent } from './components/log-in/log-in.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { NewUserComponent } from './components/new-user/new-user.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-
-
-
 import {MatToolbarModule} from '@angular/material/toolbar';
-
 import { MatSidenavModule} from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list'
-
 import { MatIconModule } from '@angular/material/icon';
-
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
 import { PpcComponent } from './components/ppc/ppc.component';
 import { InternshipComponent } from './components/internship/internship.component';
 import { LogicBuildingComponent } from './components/logic-building/logic-building.component';
 import { UpdateProfileComponent } from './components/update-profile/update-profile.component';
 import { CreateProfileComponent } from './components/create-profile/create-profile.component';
 import { RawdataComponent } from './components/rawdata/rawdata.component'
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserDataComponent } from './components/user-data/user-data.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NewUserComponent } from './components/new-user/new-user.component';
 
 @NgModule({
   declarations: [
