94a29095a7607cc4ea74453cd827eae9f353fb78
Utkarsha-Akole <83084979+Utkarsha-Akole@users.noreply.github.com>
2022-06-21 16:22:44 +0530

    index.js Updated
    PPC and rawdata api fetch

--- Backend/index.js
+++ Backend/index.js
@@ -1,50 +1,62 @@
-const express=require('express');
-const bodyparser= require('body-parser');
-const cors= require('cors');
-const mysql= require('mysql2');
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql2');
 
-const app= express();
+const app = express();
 
 app.use(cors());
 app.use(bodyparser.json());
 
 //database connection
-const db= mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'dashboard',
-    port:3306
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'dashboard',
+    port: 3306
 })
 
 // check connection
 
-db.connect(err=>{
-    if(err){console.log(err);}
-    else
-    {
+db.connect(err => {
+    if (err) { console.log(err); } else {
         console.log("connected")
     }
 })
 
 //fetch data from server
-//here "student" is the table name which is in databse
-app.get('/Rawdata',(req,res)=>{
-   let qr=' SELECT * FROM Rawdata where mobile="8421025607"' ;
-   db.query(qr,(err,result)=>{
-    if(err){console.log(err)}
-    else if(result.length>0){
-        res.send({
-            massage:"all Rawdata Data",
-            data:result
-        })
-    }
-   })
+//here "rawdata" is the table name which is in databse
+app.get('/Rawdata', (req, res) => {
+    let qr = ' SELECT * FROM Rawdata';
+    db.query(qr, (err, result) => {
+        if (err) { console.log(err) } else if (result.length > 0) {
+            res.send({
+                massage: "all Rawdata Data",
+                data: result
+            })
+        }
+    })
+});
+
+
+//fetch data from server
+//here "ppc" is the table name which is in databse
+app.get('/Ppc', (req, res) => {
+    let qr = ' SELECT * FROM Ppc';
+    db.query(qr, (err, result) => {
+        if (err) { console.log(err) } else if (result.length > 0) {
+            res.send({
+                massage: "all PPC Data",
+                data: result
+            })
+        }
+    })
 });
 
 //post Data
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("server listen on port 3000")
 })
